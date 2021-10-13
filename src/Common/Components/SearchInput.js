b06f74545fc6ee19adf081a4c370a41b3f866a1e
Hoose <hoose.muu@gmail.com>
2021-10-13 01:42:31 -0500

    ADD: Search Input Component. ADD: Shopping Cart Component.

---
+++ src/Common/Components/SearchInput.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SearchInputStyled = styled.div`
+  width: fit-content;
+  height: fit-content;
+  position: relative;
+
+  .input-search{
+    height: 30px;
+    width: 0px;
+    border-style: none;
+    padding: 10px;
+    font-size: 0.813rem;
+    letter-spacing: 2px;
+    outline: none;
+    border-radius: 0;
+    transition: all .5s ease-in-out;
+    padding-right: 40px;
+    color:#000;
+
+    ::placeholder,
+    ::-webkit-input-placeholder {
+      color:#000;
+      font-size: 0.813rem;
+      letter-spacing: 2px;
+    }
+  }
+
+  .btn-search{
+    width: 35px;
+    height: 35px;
+    border-style: none;
+    font-size: 1rem;
+    font-weight: bold;
+    outline: none;
+    cursor: pointer;
+    border-radius: 50%;
+    position: absolute;
+    right: 0px;
+    color:#000 ;
+    background-color:transparent;
+    pointer-events: painted;
+
+    .fa-search {
+      margin-top: 17px;
+    }
+  }
+
+  .btn-search:focus ~ .input-search{
+    width: 280px;
+    border-radius: 0px;
+    background-color: transparent;
+    border-bottom:1px solid rgba(255,255,255,.5);
+    transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+  }
+  .input-search:focus{
+    width: 280px;
+    border-radius: 0px;
+    background-color: transparent;
+    border-bottom:1px solid rgba(255,255,255,.5);
+    transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+  }
+`;
+
+const SearchInput = () => {
+  return (
+    <SearchInputStyled>
+      <button className="btn-search">
+        <i className="fas fa-search"></i>
+      </button>
+      <input type="text" className="input-search" placeholder="Type to search..." />
+    </SearchInputStyled>
+  );
+}
+
+export default SearchInput
---
+++ src/Common/Components/ShoppingCart.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const ShoppingCartStyled = styled.div`
+  padding-right: 1rem;
+
+  .fa-shopping-bag {
+    margin-top: 22px;
+    font-size: 1rem;
+    font-weight: bold;
+    outline: none;
+    cursor: pointer;
+  }
+
+  .badge {
+    transform: translate(30%,-140%);
+    transform-origin: 100% 0%;
+    z-index: auto;
+    padding: 0 5px;
+    color: #fff;
+    font-weight: bold;
+    font-size: 0.65rem;
+    line-height: 20px;
+    white-space: nowrap;
+    text-align: center;
+    background: #ff4d4f;
+    border-radius: 15px;
+  }
+`;
+
+const ShoppingCart = () => {
+
+  return (
+    <ShoppingCartStyled>
+      <i className="fas fa-shopping-bag"></i>
+      <div className="badge">10</div>
+    </ShoppingCartStyled>
+  )
+};
+
+export default ShoppingCart;
