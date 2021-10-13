7fe458979715537106d16585587c82b605b20f20
Hoose <hoose.muu@gmail.com>
2021-10-13 01:40:37 -0500

    ADD: Footer Component.

---
+++ src/Layouts/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const FooterStyled = styled.footer`
+  display: block;
+  position: absolute;
+  color: #808080;
+  text-align: center;
+  padding: 2rem 0rem;
+  width: 100%;
+  box-shadow: 0 -1px 2px 0 rgb(0 0 0/10%);
+`;
+
+
+const Footer = () => {
+  return (
+    <FooterStyled>
+      Ecommerce created during Wizeline’s Academy React Bootcamp
+    </FooterStyled>
+  )
+};
+
+export default Footer;
