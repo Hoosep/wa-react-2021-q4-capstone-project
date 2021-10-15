8ab150b10f06f88e9ce3f0c6edeed2452656f935
Hoose <hoose.muu@gmail.com>
2021-10-14 21:00:15 -0500

    UPD: Style Slider.

--- src/Styles/Slider.js
+++ src/Styles/Slider.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const SliderStyled = styled.div`
   width: 100%;
-  height: 100%;
+  height: calc(100% - 7.5rem);
+  max-height: 100%;
   display: flex;
   flex-direction: column!important;
   background-color: transparent;
