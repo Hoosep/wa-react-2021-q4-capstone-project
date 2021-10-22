71ff24a8d564e94673f6d3751df5cef66d279353
Hoose <hoose.muu@gmail.com>
2021-10-21 23:20:28 -0500

    ADD: Component and style pagination.

---
+++ src/Styles/Pagination.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+const PaginationStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .page {
+    font-size: 0.75rem;
+    margin: 0;
+    background: #FFF;
+    color: #101118;
+    max-width: auto;
+    min-width: 20px;
+    height: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    padding: 0.1rem;
+    &:active {
+      background-color: #101118;
+      color: #FFF;
+    }
+    @media (hover: hover) {
+      &:hover {
+        background-color: #101118;
+        color: #FFF;
+      }
+    }
+  }
+
+  .page.active {
+    background-color: #101118;
+    color: #FFF;
+    position: relative;
+    font-size: 1rem;
+  }
+`;
+
+export default PaginationStyled;
